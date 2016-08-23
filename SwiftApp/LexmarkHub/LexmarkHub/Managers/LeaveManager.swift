795862d999ce96e5ff92692ee24c6706c57cc874
Srilatha Karancheti <srilatha.karancheti@kofax.com>
2016-08-23 17:34:14 +0530

    Initial checkin of Folder Structure

---
+++ SwiftApp/LexmarkHub/LexmarkHub/Managers/LeaveManager.swift
@@ -0,0 +1,14 @@
+//
+//  LeaveManager.swift
+//  LeaveManagementSystem
+//
+//  Created by Durga on 23/08/16.
+//  Copyright © 2016 Srilatha. All rights reserved.
+//
+
+import UIKit
+
+class LeaveManager: NSObject {
+    
+
+}
