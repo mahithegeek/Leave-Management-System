1d954059e106c755d2ed24484533d1be1fe70240
Srilatha <Durga@durga.asia.kofax.com>
2016-09-06 15:28:59 +0530

    Added files required for ApplyLeave and designed UI

---
+++ SwiftApp/LexmarkHub/LexmarkHub/PresentationLayer/CustomCells/ApplyLeaveCell.swift
@@ -0,0 +1,32 @@
+//
+//  ApplyLeaveCellTableViewCell.swift
+//  LexmarkHub
+//
+//  Created by Durga on 26/08/16.
+//  Copyright © 2016 kofax. All rights reserved.
+//
+
+import UIKit
+
+class ApplyLeaveCell: UITableViewCell {
+    
+    @IBOutlet weak var fromDate: UITextField!
+    @IBOutlet weak var toDate: UITextField!
+    @IBOutlet weak var leaveType: UITextField!
+    @IBOutlet weak var startdateButton: UIButton!
+    @IBOutlet weak var endDateButton: UIButton!
+
+
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
